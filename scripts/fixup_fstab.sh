ff1e2755a93164bad2001bb12231a01ef17193ae
Auston Barboza <austonpramodh@gmail.com>
2024-03-13 21:41:31 -0500

    chore: change vm testing based command

--- scripts/fixup_fstab.sh
+++ scripts/fixup_fstab.sh
@@ -1,4 +1,4 @@
-# sed -i.orig -e "s/\(PARTUUID.*\)\(rw\)/\1ro/" /etc/fstab
+sed -i.orig -e "s/\(PARTUUID.*\)\(rw\)/\1ro/" /etc/fstab
 sed -i.orig -e "s/\(UUID.*\)\(rw\)/\1ro/" /etc/fstab # VM testing!
 echo "mount_overlay	/home	fuse	nofail,defaults	0	0" >>/etc/fstab
 echo "mount_overlay	/root	fuse	nofail,defaults	0	0" >>/etc/fstab
