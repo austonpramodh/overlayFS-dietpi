9b5ffd8cf4d02e409940c75212c4eb338de8ad9a
Auston Barboza <austonpramodh@gmail.com>
2024-03-13 20:58:42 -0500

    chore: updated sed command for fix fstab

--- scripts/fixup_fstab.sh
+++ scripts/fixup_fstab.sh
@@ -1,5 +1,5 @@
-# sed -i.orig -e "s/\(PARTUUID.*\)\(defaults\)/\1ro/" /etc/fstab
-sed -i.orig -e "s/\(UUID.*\)\(defaults\)/\1ro/" /etc/fstab # No PARTUUID in VM while testing
+# sed -i.orig -e "s/\(PARTUUID.*\)\(rw\)/\1ro/" /etc/fstab
+sed -i.orig -e "s/\(UUID.*\)\(rw\)/\1ro/" /etc/fstab # VM testing!
 echo "mount_overlay	/home	fuse	nofail,defaults	0	0" >>/etc/fstab
 echo "mount_overlay	/root	fuse	nofail,defaults	0	0" >>/etc/fstab
 echo "mount_overlay	/var	fuse	nofail,defaults	0	0" >>/etc/fstab
