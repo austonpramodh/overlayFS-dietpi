b6a3da9d89fd8028d5da7409a954cdd374b5ac0d
Auston Pramodh Barboza <austonpramodh@gmail.com>
2025-03-31 11:58:39 -0500

    fix: movefs issues

--- scripts/movefs.sh
+++ scripts/movefs.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
 TMPFS_PREFIX="/overlays"
 ORIGFS_PREFIX="/orig_fs"
 
 for DIR in /var /root /home
 do
 	TMPFS_DIR_ROOT="${TMPFS_PREFIX}${DIR}_rw"
-	ORIGFS_DIR_ROOT="${ORIGFS_PREFIX}${DIR}_org"
+	ORIGFS_DIR_ROOT="${ORIGFS_PREFIX}${DIR}"
 
-	mv -v ${D} ${ORIGFS_DIR_ROOT}
-	cd ${ORIGFS_DIR_ROOT}
+	mv -v ${DIR} ${ORIGFS_DIR_ROOT}_org
+	cd ${ORIGFS_DIR_ROOT}_org
 	find . | cpio -pdum ${ORIGFS_DIR_ROOT}_stage
-	mkdir -p -v ${D} ${TMPFS_DIR_ROOT} ${D}/.overlaysync ${ORIGFS_DIR_ROOT}/.overlaysync
+	mkdir -p -v ${DIR} ${TMPFS_DIR_ROOT} ${DIR}/.overlaysync ${ORIGFS_DIR_ROOT}_org/.overlaysync
 done
 exit
