1f9545a65263616478a9bea7cd324fe18aa24786
Auston Pramodh Barboza <austonpramodh@gmail.com>
2025-03-31 00:33:24 -0500

    chore: fix systemctl daemon reload issue and multiple tmpfs entries

--- scripts/fixup_fstab.sh
+++ scripts/fixup_fstab.sh
@@ -3,5 +3,6 @@ sed -i.orig -e "s/\(PARTUUID.*\)\(rw\)/\1ro/" /etc/fstab
 echo "mount_overlay	/home	fuse	size=512M,nofail,defaults	0	0" >>/etc/fstab
 echo "mount_overlay	/root	fuse	size=512M,nofail,defaults	0	0" >>/etc/fstab
 echo "mount_overlay	/var	fuse	size=512M,nofail,defaults	0	0" >>/etc/fstab
-echo "none		/tmp	tmpfs	defaults	0	0" >>/etc/fstab
+
+# echo "none		/tmp	tmpfs	defaults	0	0" >>/etc/fstab
 systemctl daemon-reload
